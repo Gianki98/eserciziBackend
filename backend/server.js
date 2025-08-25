8d86f922cbae610bdf16fde0b7d684f5f5a9282c
Carlo Ganci <carloganci98@gmail.com>
2025-08-25 13:03:19 +0200

    caricato esercizio html-response

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,19 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res
+    .status(200) 
+    .set("Content-Type", "text/html") 
+    .send(`<!doctype html>
+<html lang="it">
+  <head><meta charset="utf-8"><title>Home</title></head>
+  <body>
+    <h1>Server online</h1>
+  </body>
+</html>`);
+});
+
 app.listen(PORT, () =>
   console.log(`Il server è attivo su http://localhost:${PORT}`)
 );
